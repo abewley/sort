9381b39b16c65bcd79438fd3e98d16fe80206af2
Gabriel Downs <gndowns@gmail.com>
2021-10-04 14:34:53 -0400

    fix so that bounding boxes are returned in the same order they are given, instead of reversed

--- sort.py
+++ sort.py
@@ -240,7 +240,7 @@ class Sort(object):
         trk = KalmanBoxTracker(dets[i,:])
         self.trackers.append(trk)
     i = len(self.trackers)
-    for trk in reversed(self.trackers):
+    for trk in self.trackers:
         d = trk.get_state()[0]
         if (trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits):
           ret.append(np.concatenate((d,[trk.id+1])).reshape(1,-1)) # +1 as MOT benchmark requires positive
