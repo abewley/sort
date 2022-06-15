e77a7b097c42d2e09d67476a36aa97f4d4ff0155
talDrill <tal@drill-d.co.il>
2022-06-15 17:58:22 +0300

    remove unused rows

--- sort.py
+++ sort.py
@@ -20,7 +20,7 @@ from __future__ import print_function
 import os
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from skimage import io
