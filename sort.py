47c00376dac0d26868ab85bab9313611579d5a18
eduardo <eduardostimamiglio@gmail.com>
2023-05-10 08:07:20 -0300

    remove unused matplotlib line

--- sort.py
+++ sort.py
@@ -21,7 +21,7 @@ import os
 import sys
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from skimage import io
